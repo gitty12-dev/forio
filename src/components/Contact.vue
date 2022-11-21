1814cc4dab33b4aa581ffb386e66478ce3f8eed9
gitty <yamasa11136@gmail.com>
2022-11-21 17:03:15 +0900

    #4 fixed

--- src/components/Contact.vue
+++ src/components/Contact.vue
@@ -3,14 +3,24 @@
 </script>
 
 <template>
-	<div id="contact" class="d-flex justify-content-center align-items-center border">
-		<div>Contact</div>
+	<div id="contact" class="d-flex flex-column justify-content-center border">
+		<h1>~ Contact ~</h1>
+		<div class="twitter">
+			<a href="https://twitter.com/P0_ToF"><img class="twitter-img" src="../assets/twitter_icons_blue.svg" /></a>
+		</div>
 	</div>
   
 </template>
 
 <style scoped>
 #contact {
-	height: 100px;
+	/* height: 100px; */
+  padding: 0.5rem;
+}
+.twitter { 
+	margin: 1.0rem 0;
+}
+.twitter-img {
+	height: 32px;
 }
 </style>
