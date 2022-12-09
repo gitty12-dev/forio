1439275236fd923a767181ada01a51c90744558a
gitty <yamasa11136@gmail.com>
2022-12-09 14:33:59 +0900

    設定値管理

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,18 @@
 ﻿import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 
+
+// 設定値の型設定
+interface ImportMetaEnv {
+  readonly VITE_ENV_MOBILE_SIZE: string,
+}
+
+interface ImportMeta {
+  readonly env: ImportMetaEnv
+}
+
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: 'forio', //霑ｽ蜉?
+  base: 'forio', 
   plugins: [vue()]
 })
